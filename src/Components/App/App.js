442f2925e8e2c9e27fa2a0a41ec97e676e3a27ac
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-01 11:37:52 +0200

    prima versione, grafica quasi assente ma logica funziona

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,21 +2,49 @@ import React from 'react';
 import Film from '../Film/Film'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>I FILM DELLO STUDIO GHIBLI</h1>
-      <div className='main'>
-        
-        <Film />
-        <Film />
-        <Film />
-        <Film />
-        <Film />
-        <Film />
-      </div>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      loading: true
+    }
+  }
+  
+  componentDidMount(){
+    const url = 'https://ghibliapi.herokuapp.com/films'
+    fetch(url).then(response => response.json()).then(datas =>{
+        this.setState({loading: false, films: datas})
+      
+    })
+  }
+  render(){
+    let contenuto;
+    if(this.state.loading){
+      contenuto= <h1>Loading</h1>
+    }
+    else{
+      let lista = this.state.films
+      let griglia = lista.map(film =>{
+        return <Film key={film.id} title={film.title} description={film.description} director={film.director} producer={film.producer} year={film.release_date} />
+      })
+      return(
+        <div className='App'>
+          <h1>I FILM DELLO STUDIO GHIBLI</h1>
+          <div className='main'>
+            {griglia}
+          </div>
+        </div>
+      )
+    }
+    return(
+
+      <container>
+        <h1>I FILM DELLO STUDIO GHIBLI</h1>  
+        {contenuto}
+      </container>
+    )
+  
+  }
 }
 
 export default App;
--- src/Components/Film/Film.js
+++ src/Components/Film/Film.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function Film(){
+function Film(props){
     return(
         <div className='film'>
-            <h1>Castle in the Sky</h1>
-            <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+            <h1>{props.title}</h1>
+            <p>{props.description}</p>
             <div class='producerDirector'>
-                <h2>Director: Hayao Miyazaki</h2>
-                <h2>Producer: Isao Takahata</h2>
+                <h2>Director: {props.director}</h2>
+                <h2>Producer: {props.producer}</h2>
             </div>
-            <h3 class='anno'>Release year: 1986</h3>
+            <h3 class='anno'>Release year: {props.year}</h3>
         </div>
     )
 }
