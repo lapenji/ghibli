e474e96cc404fc5095f6c774843a2b0d7650a41e
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-01 10:46:09 +0200

    creati componenti di base e css

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Film from '../Film/Film'
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>I FILM DELLO STUDIO GHIBLI</h1>
+      <div className='main'>
+        
+        <Film />
+        <Film />
+        <Film />
+        <Film />
+        <Film />
+        <Film />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Film/Film.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Film(){
+    return(
+        <div className='film'>
+            <h1>Castle in the Sky</h1>
+            <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+            <div class='producerDirector'>
+                <h2>Director: Hayao Miyazaki</h2>
+                <h2>Producer: Isao Takahata</h2>
+            </div>
+            <h3 class='anno'>Release year: 1986</h3>
+        </div>
+    )
+}
+
+
+
+
+
+export default Film
